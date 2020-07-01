8874ec04ca389c71aa83c873d0c727979e73c000
Haruka Teduka <th_kiyose@outlook.com>
2020-07-01 12:09:35 +0900

    チームのオーナー権限を譲渡するowner_changeアクションを定義

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,6 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_team, only: %i[show edit update destroy]
+  before_action :set_team, only: %i[show edit update destroy owner_change]
 
   def index
     @teams = Team.all
@@ -45,6 +45,15 @@ class TeamsController < ApplicationController
 
   def dashboard
     @team = current_user.keep_team_id ? Team.find(current_user.keep_team_id) : current_user.teams.first
+  end
+
+  def owner_change
+    if current_user == @team.owner
+      @team.update(owner_id: params[:user_id])
+      redirect_to @team, notice: I18n.t('views.messages.owner_change')
+    else
+      redirect_to @team
+    end
   end
 
   private
